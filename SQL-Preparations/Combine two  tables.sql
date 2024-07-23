0ceec303c5d2897cb7f481619f54c564baa05a97
Aravind <aravindmunna240@gmail.com>
2024-07-23 14:54:06 +0530

    Q:175 Combine two tables

---
+++ SQL-Preparations/Combine two  tables.sql
@@ -0,0 +1,66 @@
+-- Question 175
+-- Table: Person
+
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | PersonId    | int     |
+-- | FirstName   | varchar |
+-- | LastName    | varchar |
+-- +-------------+---------+
+-- PersonId is the primary key column for this table.
+-- Table: Address
+
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | AddressId   | int     |
+-- | PersonId    | int     |
+-- | City        | varchar |
+-- | State       | varchar |
+-- +-------------+---------+
+-- AddressId is the primary key column for this table.
+ 
+
+-- Write a SQL query for a report that provides the following information for each person in the Person table,
+-- regardless if there is an address for each of those people:
+
+-- FirstName, LastName, City, State
+
+
+-- Input: 
+-- Person table:
+-- +----------+----------+-----------+
+-- | personId | lastName | firstName |
+-- +----------+----------+-----------+
+-- | 1        | Wang     | Allen     |
+-- | 2        | Alice    | Bob       |
+-- +----------+----------+-----------+
+-- Address table:
+-- +-----------+----------+---------------+------------+
+-- | addressId | personId | city          | state      |
+-- +-----------+----------+---------------+------------+
+-- | 1         | 2        | New York City | New York   |
+-- | 2         | 3        | Leetcode      | California |
+-- +-----------+----------+---------------+------------+
+-- Output: 
+-- +-----------+----------+---------------+----------+
+-- | firstName | lastName | city          | state    |
+-- +-----------+----------+---------------+----------+
+-- | Allen     | Wang     | Null          | Null     |
+-- | Bob       | Alice    | New York City | New York |
+-- +-----------+----------+---------------+----------+
+-- Explanation: 
+-- There is no address in the address table for the personId = 1 so we return null in their city and state.
+-- addressId = 1 contains information about the address of personId = 2.
+
+
+
+-- Solution
+
+  SELECT  p.firstName , p.lastName , a.city , a.state 
+    FROM Person AS p 
+     LEFT JOIN 
+    Address AS a
+    ON p.personId = a.personId;
+
